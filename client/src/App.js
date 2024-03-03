7f10660b17474f921cf589d4b2c011974c5a644d
Shruti Gossain <shrutigossain335@gmail.com>
2024-03-03 23:12:35 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,7 @@ import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
-import Dashboard from "scenes/dashboard"
-
+import Dashboard from "scenes/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -14,16 +13,17 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-       <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-          <Route element={<Layout />}>
+            <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+
               </Route>
           </Routes>
-          </ThemeProvider>
-          </BrowserRouter>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Layout = () => {
-  return (
-    <div>
-      Layout
-    </div>
-  )
+  <div>Layout</div>
 }
 
 export default Layout
